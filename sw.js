8d2dd3cd63b8e7aaa6c8378739d0f77b3db0ac33
dayane19santana-hub <dayane19.santana@gmail.com>
2026-08-06 20:40:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plano-v15';
+const CACHE = 'plano-v16';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
